24f8882e2e3c165d4ce735cfdff2b15a3c2278dc
Reza Jelveh <toyb0mb@gmx.net>
2013-02-02 15:36:55 -0600

    update database online

--- db_connector.py
+++ db_connector.py
@@ -6,7 +6,8 @@ import yaml
 
 connection = Connection('localhost', 27017)
 db = connection.test_database
-posts = db.posts
+db.shelters.drop()
+shelters_model = db.shelters
 URL2 = "http://lafayette.otvia.com/packet/json/allshelters"
 
 if __name__ == "__main__":
@@ -14,8 +15,14 @@ if __name__ == "__main__":
     shelters = yaml.load(allshelters)
 
     for shelter in shelters['ShelterArray']:
-        sh = {"shelter_id": shelter['Shelter']['ShelterId'],
-                "name": shelter['Shelter']['ShelterName'],
-                "latitude": shelter['Shelter']['Latitude'],
-                "longitude": shelter['Shelter']['Longitude']}
-        print sh
+        stuff = shelter['Shelter']
+        sh = {"shelter_id": stuff['ShelterId'],
+                "name": stuff['ShelterName'],
+                "latitude": stuff['Latitude']/100000.0,
+                "longitude": -stuff['Longitude']/100000.0}
+
+        # print sh
+        shelters_model.insert(sh)
+        route_ids = stuff['routeIDs']
+        for i in route_ids:
+            print i
