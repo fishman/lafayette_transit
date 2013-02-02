913ab81864df299ea213f53197cb1615335c36e7
Reza Jelveh <toyb0mb@gmx.net>
2013-02-02 16:46:08 -0600

    use mongoengine for subdocuments

--- db_connector.py
+++ db_connector.py
@@ -1,14 +1,32 @@
 from pymongo import Connection
+from mongoengine import *
 import urllib
 import json
 import pprint
 import yaml
 
-connection = Connection('localhost', 27017)
-db = connection.test_database
-db.shelters.drop()
-shelters_model = db.shelters
+# connection = Connection('localhost', 27017)
+# db = connection.test_database
+# db.shelters.drop()
+# db.routes.drop()
+# shelters_model = db.shelters
+# routes_model = db.routes
+
+connect('test_database')
+
 URL2 = "http://lafayette.otvia.com/packet/json/allshelters"
+
+
+class BusStop(EmbeddedDocument):
+    shelter_id = IntField()
+    name = StringField()
+    latitude = IntField()
+    longitude = IntField()
+
+
+class Route(Document):
+    route_id = IntField()
+    bus_stops = ListField(EmbeddedDocumentField(BusStop))
 
 if __name__ == "__main__":
     allshelters = urllib.urlopen(URL2)
@@ -16,13 +34,37 @@ if __name__ == "__main__":
 
     for shelter in shelters['ShelterArray']:
         stuff = shelter['Shelter']
-        sh = {"shelter_id": stuff['ShelterId'],
-                "name": stuff['ShelterName'],
-                "latitude": stuff['Latitude']/100000.0,
-                "longitude": -stuff['Longitude']/100000.0}
 
-        # print sh
-        shelters_model.insert(sh)
+        stop = BusStop(shelter_id=stuff['ShelterId'],
+                    name=stuff['ShelterName'],
+                    latitude=stuff['Latitude']/100000.0,
+                    longitude=-stuff['Longitude']/100000.0)
+
         route_ids = stuff['routeIDs']
         for i in route_ids:
-            print i
+            routes = Route.objects(route_id=i)
+            if len(routes) > 0:
+                route = routes[0]
+            else:
+                route = Route(route_id=i)
+
+            route.bus_stops.append(stop)
+            route.save()
+
+    print Route.objects
+
+    # for shelter in shelters['ShelterArray']:
+    #     stuff = shelter['Shelter']
+    #     sh = {"shelter_id": stuff['ShelterId'],
+    #             "name": stuff['ShelterName'],
+    #             "latitude": stuff['Latitude']/100000.0,
+    #             "longitude": -stuff['Longitude']/100000.0}
+
+    #     # print sh
+    #     shelters_model.insert(sh)
+    #     route_ids = stuff['routeIDs']
+
+    #     for i in route_ids:
+
+
+    #         print i
